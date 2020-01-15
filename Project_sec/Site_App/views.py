1bf855a32826d6ea998b559db07addd9b00b6e1d
Lena-1995 <lenachuprin4@gmail.com>
2020-01-15 14:12:08 +0200

    updates

--- Project_sec/Site_App/views.py
+++ Project_sec/Site_App/views.py
@@ -21,6 +21,11 @@ def HOME_view(request):
     # return HttpResponse('<h1>This is my home page</h1>')
 
 
+def LOGED_view(request):
+
+
+    return render(request, 'LOGIN/LOGED AS.html')
+
 
 
 def REGISTER_view(request):
@@ -35,7 +40,25 @@ def REGISTER_view(request):
         new_form = form.save()
 
     return render(request, 'LOGIN/REGISTER/Register_Page.html')
-    # return HttpResponse('<h1>This is my home page</h1>')
+
+
+
+
+
+
+def success_view(request):
+
+    form = SingInForm(request.POST or None)
+    if request.method == "POST" and form.is_valid():
+        print(request.POST)
+        print(form.cleaned_data)
+        data = form.cleaned_data
+        print(data["name"])
+
+        new_form = form.save()
+
+    return render(request, 'LOGIN/REGISTER/REGISTER SECCSES/SECSES.html')
+
 
 
 def ADMIN_view(request):
@@ -50,7 +73,88 @@ def ADMIN_view(request):
         new_form = form.save()
 
     return render(request, 'USERS/ADMIN/ADMIN_page.html')
-    # return HttpResponse('<h1>This is my home page</h1>')
+
+
+
+
+def ADMIN_SETTINGS_view(request):
+
+    form = SingInForm(request.POST or None)
+    if request.method == "POST" and form.is_valid():
+        print(request.POST)
+        print(form.cleaned_data)
+        data = form.cleaned_data
+        print(data["name"])
+
+        new_form = form.save()
+
+    return render(request, 'USERS/ADMIN/SETTINGS/SETTINGS.html')
+
+
+
+
+
+
+
+
+def ADMIN_USERS_view(request):
+
+    return render(request, 'USERS/ADMIN/USERS/USERS.html')
+
+
+
+
+
+
+
+def ADMIN_USERS_TEACHERS_CLASS_view(request):
+
+    return render(request, 'USERS/ADMIN/USERS/TEACHERS/CLASS.html')
+
+def ADMIN_USERS_TEACHERS_CLASS_STUDENTS_view(request):
+
+    return render(request, 'USERS/ADMIN/USERS/TEACHERS/CLASS/STUDENTS.html')
+
+
+
+def STUDENT_view(request):
+
+    return render(request, 'USERS/STUDENT/Student_page.html')
+
+def STUDENT_ALPHABET_view(request):
+
+    return render(request, 'USERS/STUDENT/ALPHABET/ALPHABET.html')
+
+
+def STUDENT_BOOKS_view(request):
+
+    return render(request, 'USERS/STUDENT/BOOKS/BOOKS.html')
+
+
+def STUDENT_TEST_view(request):
+
+    return render(request, 'USERS/STUDENT/TEST/TEST.html')
+
+def STUDENT_WORDS_view(request):
+
+    return render(request, 'USERS/STUDENT/WORDS/WORDS.html')
+
+
+
+
+def TEACHER_view(request):
+
+    return render(request, 'USERS/TEACHER/Teacher_page.html')
+
+
+
+
+
+
+
+
+
+
 
 
 '''
